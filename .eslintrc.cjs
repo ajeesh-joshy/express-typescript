11d5666f851c1fd38176b9ec5c9e6d02e06b4c19
Ajeesh Joshy <ajeesh.joshy@ignitarium.com>
2022-06-29 06:38:56 +0530

    First commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+      '@typescript-eslint',
+    ],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+    ],
+    "rules": {
+        "no-console": "error",
+        "no-restricted-syntax": [
+            "error",
+            {
+                "selector": "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
+                "message": "Unexpected property on console object was called"
+            }
+        ]
+    }
+  };
